67935d28e07f481a6448c15f271b511b55bd123f
Juho Kaasalainen <juho@taiste.fi>
2017-08-30 22:57:50 +0300

    Added scatter plot

--- src/taiste_presentation/core.cljs
+++ src/taiste_presentation/core.cljs
@@ -27,13 +27,15 @@
           [:p.intro-text "We tried some Coke."]]]
       [:section {:data-state "paths"}
         [:div.taistePaths]]
-      ;Drinks
+
+      ;Drink scoring
       [:section {:data-state "score-intro"}
         [:div {:class "slide text-container"}
           [:p.score-text "Coke scores"]]]
       [:section {:data-state "score"}
         [:div.colaScore]]
-      ;Drinkers
+
+      ;Drinker scoring
       [:section {:data-state "histogram-intro"}
         [:div {:class "slide text-container"}
           [:p.histogram-text "Cokée scores"]]]
@@ -42,6 +44,15 @@
       [:section {:data-state "congratz"}
         [:div {:class "slide text-container"}
           [:p.congratz-text "Congratz Fossa!"]]]
+
+      ;What to Drink?
+      [:section {:data-state "scatter-intro"}
+        [:div {:class "slide text-container"}
+          [:p.scatter-text "What you should buy."]]]
+      [:section {:data-state "scatter"}
+        [:div.colaScatter]]
+
+      ;Outro
       [:section {:data-state "outro"}
         [:div {:class "slide text-container"}
           [:p.outro-text "Thanks!"]]]]])
@@ -49,15 +60,23 @@
 (defn main-page []
   [reveal-slides])
 
-(defn event-listeners []
+(defn text-listeners []
   (.addEventListener js/Reveal "intro" #(.add (.-classList (aget (.getElementsByClassName js/document "intro-text") 0)) "visible"))
   (.addEventListener js/Reveal "score-intro" #(.add (.-classList (aget (.getElementsByClassName js/document "score-text") 0)) "visible"))
   (.addEventListener js/Reveal "histogram-intro" #(.add (.-classList (aget (.getElementsByClassName js/document "histogram-text") 0)) "visible"))
+  (.addEventListener js/Reveal "scatter-intro" #(.add (.-classList (aget (.getElementsByClassName js/document "scatter-text") 0)) "visible"))
   (.addEventListener js/Reveal "congratz" #(.add (.-classList (aget (.getElementsByClassName js/document "congratz-text") 0)) "visible"))
-  (.addEventListener js/Reveal "outro" #(.add (.-classList (aget (.getElementsByClassName js/document "outro-text") 0)) "visible"))
+  (.addEventListener js/Reveal "outro" #(.add (.-classList (aget (.getElementsByClassName js/document "outro-text") 0)) "visible")))
+
+(defn graph-listeners []
   (.addEventListener js/Reveal "paths" #(set! (.-innerHTML (aget (.getElementsByClassName js/document "taistePaths") 0)) "<object class='slide' type='text/html' data='taistePaths/index.html' ></object>"))
   (.addEventListener js/Reveal "score" #(set! (.-innerHTML (aget (.getElementsByClassName js/document "colaScore") 0)) "<object class='slide' type='text/html' data='colaMaistelu/index.html' ></object>"))
-  (.addEventListener js/Reveal "histogram" #(set! (.-innerHTML (aget (.getElementsByClassName js/document "colaHistogram") 0)) "<object class='slide' type='text/html' data='colaMaisteluHistogram/index.html' ></object>")))
+  (.addEventListener js/Reveal "histogram" #(set! (.-innerHTML (aget (.getElementsByClassName js/document "colaHistogram") 0)) "<object class='slide' type='text/html' data='colaMaisteluHistogram/index.html' ></object>"))
+  (.addEventListener js/Reveal "scatter" #(set! (.-innerHTML (aget (.getElementsByClassName js/document "colaScatter") 0)) "<object class='slide' type='text/html' data='colaMaisteluScatter/index.html' ></object>")))
+
+(defn event-listeners []
+  (text-listeners)
+  (graph-listeners))
 
 (defn main! []
   (reagent/render [main-page]
@@ -70,7 +89,7 @@
                                    :padding 0
                                    :minScale 1
                                    :maxScale 1
-                                   :viewDistance 3}))
+                                   :viewDistance 2}))
   (event-listeners))
 
 (main!)
